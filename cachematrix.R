98ac5507231e1c88c583d0beca2ca658037e82c1
chhinaaman <chhinaaman@users.noreply.github.com>
2015-07-20 16:23:33 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We are writing a R program to R function to test cache functionality for potentially time-consuming operations.
+## We will be calculating inverse of a Matrix to test how caching works
 
-## Write a short comment describing this function
+## The makeCacheMatrix function will create a vector of functions to operate on the matrix we want to inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function( x = matrix() ) {
 
+    cachedInvMatrix <- NULL
+  
+    set <- function(y) {    #Sets the original Matrix values in cache
+           x <<- y
+           cachedInvMatrix <<- NULL
+    }
+          
+  	get <- function() x  #gets the original matrix
+  
+  	#store the Inverse of the matrix in the cache
+    setInvMatrix <- function(InvMatrix) cachedInvMatrix <<- InvMatrix
+    
+    #get the cached value of the inverse of the matrix
+    getInvMatrix <- function() cachedInvMatrix
+  
+   list(set = set, get = get,
+        setInvMatrix = setInvMatrix,
+        getInvMatrix = getInvMatrix )
 }
 
+cacheSolve <- function(x) {
 
-## Write a short comment describing this function
+      
+        cachedInvMatrix <- x$getInvMatrix()
+        
+        #Check if the cache value exists for the inverse matrix
+        if(!is.null(cachedInvMatrix)) {
+                message("getting cached inverse matrix")
+                return(cachedInvMatrix)
+        }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        #get the original matrix value
+        mymatrix <- x$get()
+        
+        #use solve method to get the inverse of the matrix
+        cachedInvMatrix  <- solve(mymatrix)
+        
+        #store the inverse matrix in the cache variable
+        x$setInvMatrix(cachedInvMatrix)
+        
+        cachedInvMatrix
 }
+
